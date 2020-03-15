54c3c9a5b0743bab7629a82fcafa393f0fc972b3
Edem2000 <e.veliev@student.inha.uz>
2020-03-15 05:53:28 +0500

    minor changes

--- webpage/buyagrade.php
+++ webpage/buyagrade.php
@@ -1,56 +1,57 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Buy Your Way to a Better Education!</title>
-		<link href="buyagrade.css" type="text/css" rel="stylesheet" />
-	</head>
+<head>
+    <title>Buy Your Way to a Better Education!</title>
+    <link href="buyagrade.css" type="text/css" rel="stylesheet" />
+</head>
 
-	<body>
-		<h1>Buy Your Way to a Better Education!</h1>
+<body>
 
-		<p>
-			The rough economy, along with recent changes in University of Washington policy, now allow us to offer grades for money.  That's right!  All you need to get a 4.0 in this course is cold, hard, cash.
-		</p>
+<h1>Buy Your Way to a Better Education!</h1>
 
-		<hr />
+<p>
+    The rough economy, along with recent changes in University of Washington policy, now allow us to offer grades for money.  That's right!  All you need to get a 4.0 in this course is cold, hard, cash.
+</p>
 
-		<h2>Give Us Your Money</h2>
-		<form action="sucker.php" method="POST">
-		<dl>
-			<dt>Name</dt>
-			<dd>
-				<input type="text" name="name" />
-			</dd>
+<hr />
 
-			<dt>Section</dt>
-			<dd>
-				<select name="section" id="">
-					<option value="" selected disabled hidden>(Select section)</option>
-					<option value="MA">MA</option>
-					<option value="MB">MB</option>
-					<option value="MC">MC</option>
-					<option value="MD">MD</option>
-					<option value="ME">ME</option>
-					<option value="MF">MF</option>
-					<option value="MG">MG</option>
-					<option value="MH">MH</option>
-				</select>
-			</dd>
+<h2>Give Us Your Money</h2>
+<form action="sucker.php" method="post">
+<dl>
+    <dt>Name</dt>
+    <dd>
+        <input type="text" name="name" placeholder="John Doe" required >
 
-			<dt>Credit Card</dt>
-			<dd>
-				<input type="text" name="cardnumber" />
-			</dd>
-			<dd>
-				<input type="radio" name="card" value="visa" /> Visa
-				<input type="radio" name="card" value="matercard" /> Master Card
-			</dd>
-		</dl>
+    </dd>
 
-		<div>
-<input type="submit" name="submit" value="I am a giant sucker" />
-			</button>
-		</div>
-		</form>
-	</body>
+    <dt>Section</dt>
+    <dd>
+        <select name="section"  >
+            <option value="MA">MA</option>
+            <option value="MB">MB</option>
+            <option value="MC">MC</option>
+            <option value="MD">MD</option>
+            <option value="ME">ME</option>
+            <option value="MF">MF</option>
+            <option value="MG">MG</option>
+            <option value="MH">MH</option>
+        </select>
+    </dd>
+
+    <dt>Credit Card</dt>
+    <dd>
+        <input type="text" name="cardnum" maxlength="19" placeholder="1234 5678 9012 3456" required> <br>
+        <input type="radio" id="visa" name="card" value="Visa" checked>
+        <label for="visa">Visa</label>
+        <input type="radio" id="mast" name="card" value="MasterCard" >
+        <label for="mast">MasterCard</label><br>
+    </dd>
+</dl>
+
+<div>
+    <input type="submit" value="I am a giant sucker."/>
+
+</div>
+</form>
+</body>
 </html>
--- webpage/sucker.php
+++ webpage/sucker.php
@@ -1,76 +1,52 @@
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name=isset($_POST["name"]) ? $_POST["name"] : "";
-    $section=isset($_POST["section"]) ? $_POST["section"] : "";
-    $cardnumber=isset($_POST["cardnumber"]) ? $_POST["cardnumber"] : "";
-    $card=isset($_POST["card"]) ? $_POST["card"] : "";
-}
-$valid=true;
-
-
-if (!($_POST["name"] || $_POST["name"]===NULL)==NULL) {
-  $valid=false;
-}
-if (!($_POST["section"] || $_POST["section"]===NULL)==NULL) {
-  $valid=false;
-}
-if (!($_POST["cardnumber"] || $_POST["cardnumber"]===NULL)==NULL) {
-  $valid=false;
-}
-if (!($_POST["card"] || $_POST["card"]===NULL)==NULL) {
-  $valid=false;
-
-
-}
-  $file='sucker.txt';
-  $arr = array($name ,$section,$cardnumber,$card,"\n" );
-  $date= implode(";",$arr);
-  file_put_contents($file, $date, FILE_APPEND | LOCK_EX);
-
-
-  $currentSucker=file_get_contents($file);
-
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title>Buy Your Way to a Better Education!</title>
+<head>
+    <title>You are sucker!</title>
     <link href="buyagrade.css" type="text/css" rel="stylesheet" />
-  </head>
-
-  <body>
-<?php if($valid==true)
-{
-?>
-    <h1>Thanks, sucker!</h1>
-
-    <p>Your information has been recorded.</p>
-
-    <dl>
-      <dt>Name</dt>
-      <dd><?= $name ?></dd>
-
-      <dt>Section</dt>
-      <dd><?= $section ?></dd>
-
-      <dt>Credit Card</dt>
-      <dd><?= $cardnumber . "($card)" ?></dd>
-    </dl>
-
-    <p>Here all suckers</p>
-    <pre>
-<?=$currentSucker ?>
-    </pre>
+</head>
 <?php
+$name = $_REQUEST["name"];
+$cardnum = $_REQUEST["cardnum"];
+
+$namePattern = "/([A-Z][a-z]+.[A-Z][a-z]+)/";
+$cardnumPattern = "/(([0-9]{4}.){3}[0-9]{4})/";
+if(!preg_match($namePattern, $name) || !preg_match($cardnumPattern, $cardnum)) {
+    header("Location: doublesucker.php", TRUE, 301);
+    die();
 }
-  else
-  {
-
 ?>
+<body>
+<h1>Thanks, Sucker!</h1>
+<hr />
 
+<dl>
+    <dt>Your Name is:</dt>
+    <dd>
+        <?= $_REQUEST["name"] ?>
+    </dd>
 
-<p>Sorry.<a href="buyagrade.php">Try again?</a></p>
-<?php } ?>
-  </body>
+    <dt>Your Section is:</dt>
+    <dd>
+        <?= $_REQUEST["section"] ?>
+    </dd>
+
+    <dt>Your Credit Card is:</dt>
+    <dd>
+        <?= $_REQUEST["cardnum"] ?> (<?= $_REQUEST["card"] ?>)
+    </dd>
+</dl>
+<?php
+$data_to_put = $_REQUEST["name"] .", " . $_REQUEST["section"] .", " . $_REQUEST["cardnum"] .", " . ($_REQUEST["card"] ."\n" );
+file_put_contents("sucker.txt", $data_to_put, FILE_APPEND);
+?>
+<div>
+    <h2>Other suckers like you are:</h2>
+    <pre><?= file_get_contents("sucker.txt")?></pre>
+</div>
+<div>
+    <form action="buyagrade.php">
+    <input type="submit" value="Suck Again."/>
+    </form>
+</div>
+</body>
 </html>
